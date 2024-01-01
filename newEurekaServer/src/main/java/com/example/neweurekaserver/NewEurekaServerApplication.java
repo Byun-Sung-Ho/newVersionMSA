754bdd049d7f6099c9181841442ed27ddad010b7
201910806ByunSungHo <bsh4237@gmail.com>
2024-01-01 23:51:06 +0900

    first commit

---
+++ newEurekaServer/src/main/java/com/example/neweurekaserver/NewEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.neweurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewEurekaServerApplication.class, args);
+    }
+
+}
---
+++ newEurekaServer/src/test/java/com/example/neweurekaserver/NewEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.neweurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
