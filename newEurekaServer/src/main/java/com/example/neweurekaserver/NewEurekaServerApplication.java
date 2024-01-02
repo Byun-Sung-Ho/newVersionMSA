8f07c787ae63afa36a87658b7ce65ce33957d496
201910806ByunSungHo <bsh4237@gmail.com>
2024-01-02 23:22:50 +0900

    change setting

--- newEurekaServer/src/main/java/com/example/neweurekaserver/NewEurekaServerApplication.java
+++ newEurekaServer/src/main/java/com/example/neweurekaserver/NewEurekaServerApplication.java
@@ -2,8 +2,10 @@ package com.example.neweurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class NewEurekaServerApplication {
 
     public static void main(String[] args) {
